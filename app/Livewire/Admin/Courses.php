d71baca4b32779af97ec0f04325b21843f2366db
Nick Lorenz <developer@xniicki.de>
2024-11-19 19:16:29 +0100

    Add Admin Courses.

---
+++ app/Livewire/Admin/Courses.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Livewire\Admin;
+
+use App\Models\Course;
+use Carbon\Carbon;
+use Livewire\Component;
+
+class Courses extends Component
+{
+    public function render()
+    {
+
+
+        $courses = Course::query()
+            ->orderBy('startDate')
+            ->orderBy('startTime')
+            ->get()
+            ->map(function ($course) {
+                return [
+                    'id' => $course->id,
+                    'name' => $course->name,
+                    'formattedDate' => Carbon::parse($course->startDate)->format('d.m.Y'),
+                    'formattedTime' => Carbon::parse($course->startTime)->format('H:i'),
+                    'dayName' => $course->dayName,
+                    'sessions' => $course->sessions,
+                ];
+            });
+
+
+        return view('livewire.admin.courses', [
+            'courses' => $courses
+        ])->layout('components.layouts.admin');
+    }
+}

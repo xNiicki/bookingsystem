8cd2ae401b6d1ef5984097b906ee74f1ad95a048
Nick Lorenz <developer@xniicki.de>
2024-11-19 18:35:28 +0100

    Update web routes.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Livewire\Home;
+use App\Livewire\Register;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,4 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', Home::class);
+Route::get('/', Home::class)->name('home');
+
+Route::get('/register/{id}', Register::class)->name('register');

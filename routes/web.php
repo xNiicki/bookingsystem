b0a190d3e3d10990f996bd2ff8ade0666eb0a0b6
Nick Lorenz <developer@xniicki.de>
2024-11-19 18:50:07 +0100

    Update web routes.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Livewire\Home;
 use App\Livewire\Register;
+use App\Livewire\AdminLogin;
+use App\Livewire\Admin\Dashboard;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,3 +21,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', Home::class)->name('home');
 
 Route::get('/register/{id}', Register::class)->name('register');
+
+Route::get('/login', AdminLogin::class)->name('login');
+
+Route::get('/admin', Dashboard::class)->name('admin')->middleware('auth');
+
+Route::post('/logout', function () {
+    Auth::logout();
+    return redirect()->route('login');
+})->name('logout');

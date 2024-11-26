2b2d80c270c95ca2241d4fa1bf6c28e329f388f1
Nick Lorenz <developer@xniicki.de>
2024-11-26 21:11:11 +0100

    Translated.

--- resources/views/livewire/auth/register.blade.php
+++ resources/views/livewire/auth/register.blade.php
@@ -6,28 +6,28 @@
         <div class="col-md-6">
             <div class="card">
                 <div class="card-header">
-                    <h3 class="text-center">Register</h3>
+                    <h3 class="text-center">Registrieren</h3>
                 </div>
                 <div class="card-body">
                     <form wire:submit.prevent="submit">
                         <div class="mb-3">
                             <label for="name" class="form-label">Name</label>
-                            <input type="text" class="form-control" id="name" wire:model="name" placeholder="Enter your Name">
+                            <input type="text" class="form-control" id="name" wire:model="name" placeholder="Dein Name">
                             @error('name') <span class="text-danger">{{ $message }}</span> @enderror
                         </div>
                         <div class="mb-3">
-                            <label for="email" class="form-label">Email address</label>
-                            <input type="email" class="form-control" id="email" wire:model="email" placeholder="Enter your email">
+                            <label for="email" class="form-label">Emailaddresse</label>
+                            <input type="email" class="form-control" id="email" wire:model="email" placeholder="Deine E-Mail">
                             @error('email') <span class="text-danger">{{ $message }}</span> @enderror
                         </div>
                         <div class="mb-3">
-                            <label for="password" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="password" wire:model="password" placeholder="Enter your password">
+                            <label for="password" class="form-label">Passwort</label>
+                            <input type="password" class="form-control" id="password" wire:model="password" placeholder="Dein Passwort">
                             @error('password') <span class="text-danger">{{ $message }}</span> @enderror
                         </div>
                         <div class="mb-3">
-                            <label for="password_confirmation" class="form-label">Confirm Password</label>
-                            <input type="password" class="form-control" id="password_confirmation" wire:model="password_confirmation" placeholder="Enter your password again">
+                            <label for="password_confirmation" class="form-label">Passwort bestätigen</label>
+                            <input type="password" class="form-control" id="password_confirmation" wire:model="password_confirmation" placeholder="Bestätige dein Passwort">
                             @error('password_confirmation') <span class="text-danger">{{ $message }}</span> @enderror
                         </div>
 

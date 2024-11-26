bb1d5d774286ab769ab6cb740d0e93ce2fc1a8e9
Nick Lorenz <developer@xniicki.de>
2024-11-26 15:13:16 +0100

    Fix migration.

--- database/migrations/2024_11_24_203902_add_collumns_to_courses.php
+++ database/migrations/2024_11_24_203902_add_collumns_to_courses.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('courses', function (Blueprint $table) {
-            $table->dropColumn('end_time');
+            $table->dropColumn('endTime');
         });
     }
 };

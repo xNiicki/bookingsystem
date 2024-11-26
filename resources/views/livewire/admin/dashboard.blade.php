6c1033cbeb62db8c8c60884c0ff0ef49877d8239
Nick Lorenz <developer@xniicki.de>
2024-11-26 21:07:47 +0100

    Translated.

--- resources/views/livewire/admin/dashboard.blade.php
+++ resources/views/livewire/admin/dashboard.blade.php
@@ -1,9 +1,9 @@
 <div>
     <div class="alert alert-success" role="alert">
-        Welcome, {{ Auth::user()->name }}!
+        Willkommen, {{ Auth::user()->name }}!
     </div>
 
     <!-- Add more dashboard content here -->
 
-    <button wire:click="logout" class="btn btn-danger">Logout</button>
+    <button wire:click="logout" class="btn btn-danger">Abmelden</button>
 </div>
